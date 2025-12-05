1a8c8486b1a6277786c3d140361d57148584d1ca
baohaoliao <liaobaohao@gmail.com>
2025-12-05 19:02:27 +0100

    maxmin

--- verl/trainer/config/algorithm.py
+++ verl/trainer/config/algorithm.py
@@ -94,4 +94,4 @@ class AlgoConfig(BaseConfig):
     round_repeat: int = 8
     global_stat_est: bool = False
     max_budget_per_prompt: int | None = 32
-    min_budget_per_prompt: int | None = 1
+    min_budget_per_prompt: int | None = 2
